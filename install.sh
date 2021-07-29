9ae3a2cb9383d0e7e5e2b964a79645d077a4e2ec
Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>
2021-07-29 09:56:26 +0700

    update

--- install.sh
+++ install.sh
@@ -85,7 +85,7 @@ ln -sf $(pwd)/starship.toml $HOME/.config/starship.toml
 
 curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    mkdir -p $HOME/.config/nvim && ln -sf $(pwd)/init.vim $HOME/.config/nvim/init.vim && \
+    mkdir -p $HOME/.config/nvim && ln -sf $(pwd)/init.lua $HOME/.config/nvim/init.lua && \
         ln -sf $(pwd)/coc-settings.json $HOME/.config/nvim/coc-settings.json
 ln -sf $(pwd)/vimrc $HOME/.vimrc
 
