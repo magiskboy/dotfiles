5244df8a7aa0e5fd418bde7e39a08ea758395a71
Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>
2022-01-22 21:19:42 +0700

    chore: update null-ls

--- config/nvim/lua/lsp/javascript-lsp.lua
+++ config/nvim/lua/lsp/javascript-lsp.lua
@@ -1,6 +1,11 @@
-require'lspconfig'.tsserver.setup {}
+require("lspconfig").tsserver.setup({
+    on_attach = function(client)
+        client.resolved_capabilities.document_formatting = false
+        client.resolved_capabilities.document_range_formatting = false
+    end,
+})
 
 vim.cmd([[
-autocmd FileType javascript,typescript,typescriptreact,javascriptreact,xml,yaml,json,html,css,sass,scss,less,jinja,htmldjango setlocal ts=2 sts=2 sw=2
+    autocmd FileType javascript,typescript,typescriptreact,javascriptreact,xml,yaml,json,html,css,sass,scss,less,jinja,htmldjango setlocal ts=2 sts=2 sw=2
 ]])
 
--- config/nvim/lua/plugins/null-ls-config.lua
+++ config/nvim/lua/plugins/null-ls-config.lua
@@ -1,12 +1,19 @@
-local null_ls = require('null-ls')
+local null_ls = require('null-ls');
 
-local formatting = null_ls.builtins.formatting;
-
-local sources = {
-    formatting.eslint,
-    formatting.stylua
-}
+local formatting = null_ls.builtins.formatting
 
 null_ls.setup({
-    sources
+    on_attach = function(client)
+        if client.resolved_capabilities.document_formatting then
+            vim.cmd([[
+            augroup LspFormatting
+                autocmd! * <buffer>
+                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+            augroup END
+            ]])
+        end
+    end,
+    sources = {
+        formatting.prettier
+    },
 })
