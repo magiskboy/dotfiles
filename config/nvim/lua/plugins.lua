c052a1489c83d6bcc5bc3ed310306cd1d6f2fe32
Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>
2022-04-06 08:01:50 +0700

    update

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -33,13 +33,10 @@ return require('packer').startup(function()
 
     use 'vim-test/vim-test'
 
-    -- use 'hrsh7th/nvim-compe'
     use 'hrsh7th/vim-vsnip'
     use 'hrsh7th/cmp-nvim-lsp'
     use 'hrsh7th/cmp-buffer'
     use 'hrsh7th/cmp-path'
     use 'hrsh7th/cmp-cmdline'
     use 'hrsh7th/nvim-cmp'
-
-    use 'hrsh7th/cmp-vsnip'
 end)
