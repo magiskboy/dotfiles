f5c4ffb937d79bf26e4a0bdb7cd6019c189a311d
Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>
2022-05-02 15:10:10 +0700

    update

--- config/lite-xl/session.lua
+++ config/lite-xl/session.lua
@@ -1,1 +1,1 @@
-return {recents={[1]="/home/nkthanh/Development/teko-web"}, window={[1]=1920,[2]=1040,[3]=1920,[4]=40,["n"]=4}, window_mode="normal", previous_find={}, previous_replace={}}
+return {recents={[1]="/home/nkthanh/Development/teko-web"}, window={[1]=943,[2]=1036,[3]=2886,[4]=33,["n"]=4}, window_mode="normal", previous_find={}, previous_replace={}}
