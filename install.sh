31ec93946d881276e9c1db95b31a48f844b81e55
Nguyen Khac Thanh <thanh.nk@teko.vn>
2022-07-10 06:21:39 +0700

    update

--- install.sh
+++ install.sh
@@ -3,6 +3,89 @@
 LOCAL=$HOME/.local
 [[ ! -d $LOCAL/bin ]] && mkdir $LOCAL/bin
 
+sudo pacman -Sy --noconfirm
+
+sudo pacman -S --noconfirm \
+    git \
+    base-devel \
+    ripgrep \
+    htop \
+    fzf \
+    pgcli \
+    mycli \
+    zsh \
+    bat \
+    jq \
+    yq \
+    tmux \
+    docker.io \
+    dunst \
+    i3-gaps \
+    i3status-rust \
+    alsa-utils \
+    wpa_supplicant \
+    wireless_tools \
+    amd-ucode \
+    b43-fwcutter \
+    base \
+    bind \
+    brltty \
+    broadcom-wl \
+    btrfs-progs \
+    ddrescue \
+    dhclient \
+    dhcpcd \
+    diffutils \
+    dmidecode \
+    dmraid \
+    dnsmasq \
+    dosfstools \
+    e2fsprogs \
+    edk2-shell \
+    efibootmgr \
+    espeakup \
+    ethtool \
+    exfatprogs \
+    f2fs-tools \
+    gnu-netcat \
+    gpart \
+    gptfdisk \
+    grub \
+    hdparm \
+    hyperv \
+    intel-ucode \
+    ipw2100-fw \
+    ipw2200-fw \
+    iw \
+    iwd \
+    libusb-compat \
+    linux \
+    lvm2 \
+    man-db \
+    man-pages \
+    memtest86+ \
+    nmap \
+    vpnc \
+    openvpn \
+    openconnect \
+    openssh \
+    qemu-guest-agent \
+    rsync \
+    rxvt-unicode-terminfo \
+    syslinux \
+    systemd-resolvconf \
+    usb_modeswitch \
+    usbmuxd \
+    usbutils \
+    virtualbox \
+    virtualbox-guest-utils-nox \
+    wireless-regdb
+
+git clone https://aur.archlinux.org/yay-bin.git /tmp/yay && \
+cd /tmp/yay && \
+makepkg -si && \
+cd -
+
 DOCKER_CONFIG="$HOME/.docker"
 groupadd -f docker && \
 usermod -aG docker $USER && \
@@ -10,20 +93,6 @@ usermod -aG docker $USER && \
 chown "$USER":"$USER" $DOCKER_CONFIG -R && \
 chmod g+rwx $DOCKER_CONFIG -R && \
 systemctl restart docker
-
-sudo pacman -Sy i3-gaps i3status-rust alsa-utils wpa_supplicant wireless_tools
- 
-sudo pacman -Sy \
-    ripgrep \
-    htop \
-    fzf \
-    pgcli \
-    mycli \
-    httpie \
-    zsh \
-    jq \
-    docker.io \
-    dunst
 
 # Link dotfiles
 rm -rf $HOME/.zshrc $HOME/.oh-my-zsh $HOME/.zshrc $HOME/.zshenv $HOME/.zlogin $HOME/.zprofile && \
@@ -48,6 +117,5 @@ ln -sf `pwd`/pyenv $HOME/.pyenv
 
 ln -sf `pwd`/nvm $HOME/.nvm
 
-# curl https://sh.rustup.rs -sSf | sh
-
+sudo mkdir -f /usr/share/fonts && \
 sudo cp -R `pwd`/fonts /usr/share/fonts/Meslo
