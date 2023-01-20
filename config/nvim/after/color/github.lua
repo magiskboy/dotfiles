c5ace19f21730024e84039f8ddf01ed29a412cc5
Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>
2023-01-21 06:00:39 +0700

    chore:update

--- config/nvim/after/color/github.lua
+++
@@ -1,5 +0,0 @@
-function MyColor()
-    vim.nvim_set_hl(0, 'lualine_a_diff_modified_a_normal', { 'guifg' = 'blue', default = true })
-end)
-
-MyColor()
---
+++ config/nvim/after/plugin/colors.lua
@@ -0,0 +1,18 @@
+function MyColor()
+    vim.cmd[[
+        hi lualine_a_diff_modified_normal   guifg=#0043b8
+        hi lualine_a_diff_modified_insert   guifg=#0043b8
+        hi lualine_a_diff_modified_visual   guifg=#0043b8
+        hi lualine_a_diff_modified_command  guifg=#0043b8
+        hi lualine_a_diff_modified_terminal guifg=#0043b8
+        hi lualine_a_diff_modified_replace  guifg=#0043b8
+
+        hi lualine_a_diff_added_normal   guifg=#2e4233
+        hi lualine_a_diff_added_insert   guifg=#2e4233
+        hi lualine_a_diff_added_visual   guifg=#039c4d
+        hi lualine_a_diff_added_command  guifg=#039c4d
+        hi lualine_a_diff_added_terminal guifg=#039c4d
+        hi lualine_a_diff_added_replace  guifg=#039c4d
+    ]]
+end
+MyColor()
